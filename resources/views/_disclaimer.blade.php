d3af965b9f29fbe624edbb7a530834d9d92e5665
jaonoctus <joaodias@noctus.org>
2017-10-28 03:46:51 -0200

    Update disclaimer text

--- resources/views/_disclaimer.blade.php
+++ resources/views/_disclaimer.blade.php
@@ -2,6 +2,6 @@
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
-    - As informações abaixo são retiradas do site <a href="http://campuse.ro" target="_blank">campuse.ro</a> de forma automática (através de um Web Scraper) e atualizadas a cada 12h;<br>
+    - As informações abaixo são retiradas do site <a href="http://campuse.ro" target="_blank">campuse.ro</a> (através de um Web Scraper) e atualizadas a cada 12h de forma automática;<br>
     - Este ranking (não oficial) é baseado apenas no número de votos, sem validação dos perfis que se inscreveram nas atividades.
 </div>
