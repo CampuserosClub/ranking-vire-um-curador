312fd8bf1c5d8aa7a9e217f62c28acd2965d40ac
João Paulo Polles <joaopaulopolles@gmail.com>
2018-12-12 13:44:42 -0200

    correções no código

--- resources/views/_disclaimer.blade.php
+++ resources/views/_disclaimer.blade.php
@@ -4,5 +4,5 @@
     </button>
     - As informações abaixo são retiradas do site <a href="http://campuse.ro" target="_blank">campuse.ro</a> (através de um Web Scraper), atualizadas de forma automática e armazenadas em cache por 12h.<br>
     - Este ranking (<b>não oficial</b>) é baseado apenas no número de votos, sem validação dos perfis que se inscreveram nas atividades.<br>
-    - Serão escolhidas para a programação oficial do evento no mínimo​ ​15​ ​atividades​ ​mais​ ​votadas. A <b>Nota de corte</b> representa a quantidade de votos da 15º posição.
+    - Serão escolhidas para a programação oficial do evento no mínimo​ 20​ ​atividades​ ​mais​ ​votadas. A <b>Nota de corte</b> representa a quantidade de votos da 20º posição.
 </div>
