db9e47c5d7b31596cc55be28ecc795c79f4b612a
João Paulo Polles <joaopaulopolles@gmail.com>
2018-12-12 13:50:03 -0200

    correções no código

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Ranking - Vire um Curador #CPBR12</title>
+    <title>Ranking Não Oficial - Vire um Curador #CPBR12</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
